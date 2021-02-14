5dd2ba02d8921f946f388035cfaa9405cc08827a
Lee Morgan <Morgan.LeeR@protonmail.com>
2021-02-14 16:00:17 +0300

    Add transactions model.

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,6 @@
+const User = require("../models/user.js");
+const Transaction = require("../models/transaction.js");
+
 module.exports = {
     render: function(req, res){
         res.render("./index.ejs");
---
+++ models/transaction.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+let isSanitary = (str)=>{
+    let disallowed = ["\\", "<", ">", "$", "{", "}", "(", ")"];
+
+    for(let j = 0; j < disallowed.length; j++){
+        if(str.includes(disallowed[j])){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+let TransactionSchema = new mongoose.Schema({
+    account: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user.accounts",
+        required: [true, "TRANSACTION MUST BE ASSOCIATED WITH AN ACCOUNT"]
+    },
+    category: {
+        type: String,
+        required: [true, "TRANSACTION MUST HAVE A CATEGORY"]
+    },
+    amount: {
+        type: Number,
+        required: [true, "TRANSACTION MUST HAVE AN AMOUNT"]
+    },
+    location: {
+        type: String,
+        validate: {
+            validator: isSanitary,
+            message: "LOCATION CONTAINS ILLEGAL CHARACTERS"
+        }
+    },
+    date: {
+        type: Date,
+        required: [true, "TRANSACTION MUST CONTAIN A DATE"]
+    },
+    note: {
+        type: String,
+        validate: {
+            validator: isSanitary,
+            message: "NOTE CONTAINS ILLEGAL CHARACTERS"
+        }
+    }
+});
+
+module.exports = mongoose.model("transaction", TransactionSchema);
