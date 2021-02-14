cc1f8990ba00ec03272b4ec605f6029baca7e348
Lee Morgan <Morgan.LeeR@protonmail.com>
2021-02-14 17:02:42 +0300

    Create frontend function to register a user.

--- views/js/modals/enter.js
+++ views/js/modals/enter.js
@@ -1,6 +1,41 @@
 let enter = {
     display: function(){
-        console.log("enter");
+        document.getElementById("registerForm").onsubmit = ()=>{this.register()};
+        document.getElementById("loginform").onsubmit = ()=>{this.login()};
+    },
+
+    register: function(){
+        event.preventDefault();
+
+        let data = {
+            email: document.getElementById("registerEmail").value,
+            password: document.getElementById("registerPass").value,
+            confirmPassword: document.getElementById("registerConfirm").value;
+        }
+
+        if(data.password !== data.confirmPassword){
+            return;
+        }
+
+        fetch("/register", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8"
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response = response.json())
+            .then((response)=>{
+                if(typeof(response) === "string"){
+                    throw response;
+                }
+
+                user = response;
+                //open create account modal
+            })
+            .catch((err)=>{
+                //createBanner
+            })
     }
 }
 
