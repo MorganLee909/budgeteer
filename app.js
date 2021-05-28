790564d39e57f43cdbc81f5b30863a908f5c3ff6
Lee Morgan <me@leemorgan.io>
2021-05-28 12:01:17 +0300

    Switch from browserify to esbuild.

--- app.js
+++ app.js
@@ -2,22 +2,31 @@ const express = require("express");
 const session = require("cookie-session");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const esbuild = require("esbuild");
 const https = require("https");
 const fs = require("fs");
 
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/views"));
+app.use(express.static(`${__dirname}/views`));
 
-let httpsServer = {};
 let mongooseOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true,
     dbName: "budgeteer"
-}
+};
+
+let esbuildOptions = {
+    entryPoints: ["./views/js/index.js"],
+    bundle: true,
+    minify: false,
+    outfile: "./views/bundle.js"
+};
+
+let httpsServer = {};
 if(process.env.NODE_ENV === "production"){
     httpsServer = https.createServer({
         key: fs.readFileSync("/etc/letsencrypt/live/budgeteer.money/privkey.pem", "utf8"),
@@ -35,13 +44,15 @@ if(process.env.NODE_ENV === "production"){
     mongooseOptions.auth = {authSource: "admin"};
     mongooseOptions.user = "website";
     mongooseOptions.pass = process.env.MONGODB_PASS;
+    cssOptions.minimize = true;
+    esbuildOptions.minify = true;
 }
 
 mongoose.connect("mongodb://127.0.0.1:27017/", mongooseOptions);
 
 app.use(compression());
 app.use(session({
-    secret: "Balancing budgets believably",
+    secret: "Balancing budgets believably by brow-beating buyers",
     cookie: {secure: true},
     saveUninitialized: true,
     resave: false
